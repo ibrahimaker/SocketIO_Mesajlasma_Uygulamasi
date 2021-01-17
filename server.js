994b584b4bdd1660cd7f90b84680e3d16a7a79e7
İbrahim Samed AKER <ibrahimsamedaker@gmail.com>
2021-01-17 23:57:07 +0300

    Socket.io Mesajlaşma Uygulaması
    Socket.io Mesajlasma Socket.io Mesajlaşma UygulamasıUygulaması

---
+++ server.js
@@ -0,0 +1,44 @@
+var app = require('express')(); // express kütüphanesini projeye dahil ediyoruz
+var http = require('http').createServer(app); // bir server yaratıyoruz
+const io = require('socket.io')(http, { //socket.io kütüphanesini dahil ediyoruz
+    cors: {
+      origin: '*', // cors hatası almamak için ekliyoruz.
+      }
+});
+
+
+app.get('/', (req, res)=>{
+  // web browser'dan server sayfasını açınca bu mesajı ekrana basar
+    res.send('<h1>Merhaba Dünyalı Biz Dostuz</h1>');
+});
+
+
+io.on('connection', (socket) => {
+// yeni bir connection kurulunca buraya düşer
+// socket.id'yi log olarak atıyoruz
+      console.log('socket: ' + socket.id);
+// client tarafındaki tüm kullanıcılara "yeni kullanıcı" bilgisini gönderiyoruz
+// client tarafında da socket.on(UserConnected) ile dinliyoruz
+      socket.emit("UserConnected", socket.id);
+
+ //client'dan gelecek mesajları dinliyoruz   
+      socket.on("message", function(msg){
+        console.log('socket: ' + msg);
+        // aynı mesajı tüm bağlanan kullanıcılara gönderiyoruz.
+        // bu şekilde mesaj bir yerden geliyor, bir çok yere dağıtılıyor.
+        io.emit("message", msg);
+    });
+      
+  });
+
+  io.on("disconnect", () => {
+    //socket ara ara kopabiliyor, disconnect olması durumunda burada düşer
+    console.info('disconnected socket: ', socket.id);
+});
+
+
+
+http.listen(3000, ()=>{
+  // 3000 portunu dinliyoruz
+    console.log('listening on *:3000');
+});
